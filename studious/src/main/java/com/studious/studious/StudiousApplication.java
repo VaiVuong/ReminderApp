8fe1e24a923aa3c320969e70f2569d89b338de75
Chris V <creevue@gmail.com>
2026-03-11 01:43:26 -0400

    Initial Setup for Springboot/Maven file structures and placeholder html.

---
+++ studious/src/main/java/com/studious/studious/StudiousApplication.java
@@ -0,0 +1,13 @@
+package com.studious.studious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiousApplication.class, args);
+	}
+
+}
